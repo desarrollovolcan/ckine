322cae4f6cda0938547a51307e9e1406392ed9de
desarrollovolcan <eisla@fvolcan.cl>
2026-02-03 03:56:30 -0300

    Limit menu to clinical and admin groups

--- partials/horizontal-nav.php
+++ partials/horizontal-nav.php
@@ -4,93 +4,18 @@
         <nav class="container-fluid">
             <div class="collapse navbar-collapse" id="topnav-menu-content">
                 <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php">
-                            <span class="menu-icon"><i data-lucide="layout-dashboard"></i></span>
-                            <span class="menu-text">Dashboard</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="projects.php">
-                            <span class="menu-icon"><i data-lucide="briefcase"></i></span>
-                            <span class="menu-text">Proyectos</span>
-                        </a>
-                    </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle drop-arrow-none" href="#" id="topnav-crm" data-bs-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                            <span class="menu-icon"><i data-lucide="handshake"></i></span>
-                            <span class="menu-text">CRM</span>
+                        <a class="nav-link dropdown-toggle drop-arrow-none" href="#" id="topnav-users" data-bs-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                            <span class="menu-icon"><i data-lucide="users"></i></span>
+                            <span class="menu-text">Usuarios</span>
                             <div class="menu-arrow"></div>
                         </a>
-                        <div class="dropdown-menu" aria-labelledby="topnav-crm">
-                            <a href="crm-reports.php" class="dropdown-item">Reports &amp; Insights</a>
-                            <a href="crm-briefs.php" class="dropdown-item">Briefs Comerciales</a>
-                            <a href="crm-pipeline.php" class="dropdown-item">Pipeline</a>
-                            <a href="crm-deals.php" class="dropdown-item">Deals</a>
-                            <a href="crm-orders.php" class="dropdown-item">Órdenes de Venta</a>
-                            <a href="crm-leads.php" class="dropdown-item">Leads</a>
-                            <a href="crm-campaign.php" class="dropdown-item">Campaign</a>
-                            <a href="crm-proposals.php" class="dropdown-item">Proposals</a>
-                            <a href="crm-estimations.php" class="dropdown-item">Estimations</a>
-                            <a href="crm-customers.php" class="dropdown-item">Customers</a>
-                            <a href="crm-renewals.php" class="dropdown-item">Renovaciones</a>
-                            <a href="crm-activities.php" class="dropdown-item">Activities</a>
-                            <a href="tickets-list.php" class="dropdown-item">Service Desk</a>
-                            <a href="invoices.php" class="dropdown-item">Invoicing</a>
-                            <a href="users-roles.php" class="dropdown-item">Users &amp; Roles</a>
+                        <div class="dropdown-menu" aria-labelledby="topnav-users">
+                            <a href="users-contacts.php" class="dropdown-item">Usuarios</a>
+                            <a href="users-roles.php" class="dropdown-item">Roles</a>
+                            <a href="users-role-details.php" class="dropdown-item">Detalle de rol</a>
+                            <a href="users-permissions.php" class="dropdown-item">Permisos</a>
                         </div>
-                    </li>
-
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle drop-arrow-none" href="#" id="topnav-drop" data-bs-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                            <span class="menu-icon"><i data-lucide="chevrons-down-up"></i></span>
-                            <span class="menu-text">Dropdown</span>
-                            <div class="menu-arrow"></div>
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="topnav-drop">
-                            <a href="#!" class="dropdown-item">Drop Item 1</a>
-                            <a href="#!" class="dropdown-item">Drop Item 2</a>
-                            <a href="#!" class="dropdown-item disabled">Drop Item Disabled <span class="badge text-bg-light opacity-50">soon</span></a>
-                        </div>
-                    </li>
-
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle drop-arrow-none" href="#" id="topnav-menu-levels" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span class="menu-icon"><i data-lucide="list-tree"></i></span>
-                            <span class="menu-text">Menu Levels</span>
-                            <div class="menu-arrow"></div>
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="topnav-menu-levels">
-                            <a href="#!" class="dropdown-item"><i data-lucide="ice-cream-cone"></i> Single Item</a>
-                            <div class="dropdown">
-                                <a class="dropdown-item dropdown-toggle drop-arrow-none" href="#" id="topnav-ecommerce" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <i data-lucide="workflow"></i> Second Level <div class="menu-arrow"></div>
-                                </a>
-                                <div class="dropdown-menu" aria-labelledby="topnav-ecommerce">
-                                    <a href="#!" class="dropdown-item">Third Level 1</a>
-                                    <a href="#!" class="dropdown-item">Third Level 2</a>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle drop-arrow-none" href="#" id="topnav-layouts" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span class="menu-icon"><i data-lucide="panels-top-left"></i></span>
-                            <span class="menu-text">Layouts</span>
-                            <div class="menu-arrow"></div>
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="topnav-layouts">
-                            <a href="index.php" class="dropdown-item">Vertical</a>
-                            <a href="layouts-horizontal.php" class="dropdown-item">Horizontal</a>
-                        </div>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#!">
-                            <span class="menu-icon"><i data-lucide="shield-alert"></i></span>
-                            <span class="menu-text">Disabled</span>
-                        </a>
                     </li>
                 </ul>
 
