e298d4eeec4eb520d969c097b46dbf1c23c06974
desarrollovolcan <eisla@fvolcan.cl>
2026-02-03 02:27:56 -0300

    re

--- config/db.php
+++ config/db.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'host' => '127.0.0.1',
-    'name' => 'kinecico',
+    'host' => 'localhost',
+    'name' => 'ckine',
     'user' => 'root',
     'pass' => '',
     'charset' => 'utf8mb4',
